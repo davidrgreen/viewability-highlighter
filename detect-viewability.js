bf1d0a27364d48d991d9acf647b6826da9a89625
David Green <coding@davidrg.com>
2017-09-02 09:25:14 -0400

    Create new stylesheet element to receive styles instead of using first stylesheet on page

--- detect-viewability.js
+++ detect-viewability.js
@@ -1,48 +1,53 @@
-/**
- * TODO:
- * - use https://davidwalsh.name/add-rules-stylesheets to ensure we don't insert this into a print stylesheet
- */
-
-
 var DRGAdViewablilityIndicator = ( function() {
 	'use strict';
 
-	var stylesheetReference = document.styleSheets[0];
 	var adElementStyle = 'position: relative;';
 	var adBeforePseudoElementStyle = 'content: ""; display: block; height: 100%; width: 100%; position: absolute; z-index: 5000; top: 0; left: 0; background: green; opacity: 0.65;';
 	var adAfterPsuedoElementStyle = 'content: "Ad Viewable"; display: inline-block; position: absolute; z-index: 5001; top: calc( 50% - 10px ); left: calc(50% - 55px );font-size: 16px; font-weight: bold; text-transform: uppercase; color: #fff; text-shadow: -1px -1px #000; font-family: sans-serif;';
 
-	var googletag = window.googletag || {};
-	googletag.cmd = googletag.cmd || [];
+	window.googletag = window.googletag || {};
+	window.googletag.cmd = window.googletag.cmd || [];
 
 	var setupViewableDetection = function() {
 		if ( ! window.googletag || ! window.googletag.pubadsReady ) {
 			setTimeout( setupViewableDetection, 20 );
 			return;
 		}
-		googletag.cmd.unshift( function() {
-			googletag.pubads().addEventListener( 'impressionViewable', function( viewed ) {
-				var slotElementID = viewed.slot.getSlotElementId();
-				console.log( slotElementID + ' slot now considered viewed:' );
 
-				insertStyles( slotElementID );
+		var stylesheet = createStylesheet();
+
+		window.googletag.cmd.push( function() {
+			window.googletag.pubads().addEventListener( 'impressionViewable', function( viewed ) {
+				var slotElementID = viewed.slot.getSlotElementId();
+				insertStyles( slotElementID, stylesheet );
 			} );
 		} );
 	};
 
-	var insertStyles = function( elementID ) {
-		stylesheetReference.insertRule(
+	var createStylesheet = function() {
+		var style = document.createElement( 'style' );
+
+		// Required for Webkit.
+		style.appendChild( document.createTextNode( '' ) );
+
+		document.head.appendChild( style );
+
+		return style.sheet;
+	};
+
+	var insertStyles = function( elementID, stylesheet ) {
+		stylesheet.insertRule(
 			'#' + elementID + ' { ' +
 			adElementStyle + '}',
 			0
 		);
-		stylesheetReference.insertRule(
+		stylesheet.insertRule(
 			'#' + elementID + '::before { ' +
 			adBeforePseudoElementStyle + '}',
 			0
 		);
 		if ( document.getElementById( elementID ).offsetHeight > 20 ) {
-			stylesheetReference.insertRule(
+			stylesheet.insertRule(
 				'#' + elementID + '::after { ' +
 				adAfterPsuedoElementStyle + '}',
 				0
